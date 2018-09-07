18768c4544eb18cf64477cd4ca45409e61bc7815
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-09-07 13:12:56 +0100

    58883 - EF model implemented.

---
+++ src/ESFA.DC.Data.AppsEarningsHistory.Model/ApprenticeshipsEarningsHistoryConstructor.cs
@@ -0,0 +1,10 @@
+﻿namespace ESFA.DC.Data.AppsEarningsHistory.Model
+{
+    public partial class ApprenticeshipsEarningsHistory
+    {
+        public ApprenticeshipsEarningsHistory(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+}
