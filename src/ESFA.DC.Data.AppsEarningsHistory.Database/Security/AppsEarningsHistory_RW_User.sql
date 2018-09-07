860c8f3a9baf08163450f8967a8eb22511d2a4e4
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-09-07 12:43:36 +0100

    58883 - DB creation

---
+++ src/ESFA.DC.Data.AppsEarningsHistory.Database/Security/AppsEarningsHistory_RW_User.sql
@@ -0,0 +1,7 @@
+﻿CREATE USER [AppsEarningsHistory_RW_User]
+    WITH PASSWORD = N'$(RWUserPassword)';
+GO
+	GRANT CONNECT TO [AppsEarningsHistory_RW_User]
+GO
+
+
---
+++ src/ESFA.DC.Data.AppsEarningsHistory.Database/dbo/Stored Procedures/PrepareForNewData.sql
@@ -0,0 +1,13 @@
+﻿  CREATE PROCEDURE [dbo].[PrepareForNewData] 
+	( @pCollectionYear int, @pCollectionReturnCode varchar(3),@pUKPRN int ) AS 
+	BEGIN
+  DELETE
+  FROM [dbo].[AppsEarningsHistory]
+  WHERE [CollectionYear]=@pCollectionYear
+    AND [CollectionReturnCode]=@pCollectionReturnCode
+    AND [UKPRN]=@pUKPRN
+    UPDATE [dbo].[AppsEarningsHistory]
+    SET [LatestInYear]=0 WHERE [LatestInYear]=1
+    AND [CollectionYear]=@pCollectionYear
+    AND [UKPRN]=@pUKPRN 
+END 
